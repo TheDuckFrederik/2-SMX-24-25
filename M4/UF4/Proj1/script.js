21f3e34f386880bc3b3459567421eb1431c8663e
FrederikTheDuck <pujolunai@proton.me>
2025-04-01 11:27:03 +0200

    M4/Proj1

---
+++ M4/UF4/Proj1/script.js
@@ -0,0 +1,7 @@
+function toggleSidebar() {
+    document.getElementById("sidebar").classList.toggle("open");
+}
+
+function closeSidebar() {
+    document.getElementById("sidebar").classList.remove("open");
+}
--- M4/UF4/UF4.md
+++ M4/UF4/UF4.md
@@ -1,3 +1,7 @@
 # [M4](/M4/M4.md) UF4
 ***
-## [Act1](/M4/UF4/Act1/Act1.md)
+## Activitats
+### [Act1](/M4/UF4/Act1/Act1.md)
+***
+## Projectes
+### [Projecte 1](/M4/UF4/Proj1/Proj1.html)
